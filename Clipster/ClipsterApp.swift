f217bb818a00806e811afbf3f474228c729f3337
ReneKumic <renekumic1000@gmail.com>
2026-02-17 19:29:10 +0100

    Initial Commit

---
+++ Clipster/ClipsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipsterApp.swift
+//  Clipster
+//
+//  Created by Rene Kumić on 17.02.2026..
+//
+
+import SwiftUI
+
+@main
+struct ClipsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipsterTests/ClipsterTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipsterTests.swift
+//  ClipsterTests
+//
+//  Created by Rene Kumić on 17.02.2026..
+//
+
+import Testing
+@testable import Clipster
+
+struct ClipsterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
